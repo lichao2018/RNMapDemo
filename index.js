57f4fd7ddcc0281a3fb649ddf8eab6160fb74f9f
xiaoc63 <13070153251@163.com>
2018-06-05 00:00:12 +0800

    tabview中加入地图

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import {
     AppRegistry,
     Text,
+    View
 } from 'react-native';
 import ScrollableTabView from 'react-native-scrollable-tab-view';
+import BaiduMapDemo from './BaiduMapDemo'
 
 class RNMapDemo extends Component {
     render() {
         return (
             <ScrollableTabView tabBarPosition='bottom'>
-                <Text tabLabel={'tab1'}>Text1</Text>
-                <Text tabLabel={'tab2'}>Text2</Text>
-                <Text tabLabel={'tab3'}>Text3</Text>
+                <Text tabLabel={'停车场'}>Text1</Text>
+                <BaiduMapDemo tabLabel={'导航'}/>
+                <Text tabLabel={'个人中心'}>Text3</Text>
             </ScrollableTabView>
         );
     }
