2ac2908482dc100ed60980596b2bf2f559e7c6c7
lichao2018 <290288809@qq.com>
2018-06-08 18:05:24 +0800

    按钮添加点击事件

--- android/app/src/main/java/com/rnmapdemo/MainApplication.java
+++ android/app/src/main/java/com/rnmapdemo/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactnativecomponent.amap.RCTAMapPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new AMap3DPackage()
+          new AMap3DPackage(),
+          new RCTAMapPackage()
       );
     }
 
--- app/search/search.js
+++ app/search/search.js
@@ -1,23 +1,59 @@
 import React, {Component} from 'react';
-import {TextInput} from 'react-native';
+import {
+    TextInput,
+    Button,
+    View
+} from 'react-native';
 
 export default class Search extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            searchArray : '',
+            searchLocation : []
+        }
+    }
     render(){
         return(
-            <TextInput
-                style={{
-                    height:40,
-                    borderColor:'gray',
-                    borderWidth:1,
-                    fontSize:20,
-                    padding:0,
-                    textAlignVertical:'top',
-                    margin:10
-                }}
-                underlineColorAndroid={'transparent'}
-                multiline={true}
-                placeholder={' 查找停车场'}
-            />
+            <View
+                flexDirection='row'
+            >
+                <TextInput
+                    style={{
+                        width:300,
+                        height:40,
+                        borderColor:'gray',
+                        borderWidth:1,
+                        fontSize:20,
+                        padding:0,
+                        textAlignVertical:'top',
+                        margin:10
+                    }}
+                    underlineColorAndroid={'transparent'}
+                    multiline={true}
+                    placeholder={'请输入搜索内容'}
+                    onChangeText={
+                        (value)=>this.setState({
+                            searchArray : value
+                        })
+                    }
+                />
+                <Button
+                    onPress={
+                        ()=>alert(this.getSearchLocation())
+                    }
+                    title='搜索'
+                />
+            </View>
         );
+    }
+
+    getSearchLocation(){
+        return fetch('http://restapi.amap.com/v3/place/text?key=4814285eeea3dfc092c2d1bde493f6cc&keywords={this.state.searchArray}')
+            .then((response)=>response.json)
+            .then((responseJson)=>{
+                return responseJson.entr_location;
+            })
+            .catch((error)=>console.error(error));
     }
 }
